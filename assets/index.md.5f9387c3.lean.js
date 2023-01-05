7bd6edbafa9126ba30f56a228e7d5ca9785d838b
curder <curder@users.noreply.github.com>
2023-01-05 08:22:11 +0000

    deploy: 8b85e8b2a11354e540a37de6d28ba8cca456172f

---
+++ assets/index.md.5f9387c3.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as o}from"./app.2806a809.js";const h=JSON.parse('{"title":"YAML 学习","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","lastUpdated":1672906903000}'),n={name:"index.md"},r=o("",9),i=[r];function s(d,c,l,p,_,u){return a(),t("div",null,i)}const A=e(n,[["render",s]]);export{h as __pageData,A as default};
